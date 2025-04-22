5321ee938c7c158164f29cde210b2f1eea3d2733
João Campos <joaon.c.jv@gmail.com>
2025-04-22 13:51:11 -0300

    adicionando projeto base - dart/flutter

---
+++ android/app/src/main/kotlin/com/example/rede_confeitarias/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rede_confeitarias
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:rede_confeitarias/home_page.dart';
+import 'package:rede_confeitarias/home_page_full.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePageFull(),
+  ));
+}
