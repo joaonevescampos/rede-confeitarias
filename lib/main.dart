990ab1ad9039524e417fe9058c7646700fb787d0
João Campos <joaon.c.jv@gmail.com>
2025-04-28 13:08:06 -0300

    mudança de rota /teste para /

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: ' Redes de Confeitarias',
       theme: AppTheme.lightTheme,
       onGenerateRoute: AppRouter.generateRoute,
-      initialRoute: '/teste',
+      initialRoute: '/',
       debugShowCheckedModeBanner: false,
     );
   }
